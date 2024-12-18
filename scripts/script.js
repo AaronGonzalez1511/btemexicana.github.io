5ddf5bc67c129f0f151dd523400c0d64782612c3
BTEMexicana <aaronglez302@gmail.com>
2024-12-18 15:38:29 -0600

    Descripcion de los cambios realizados

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,18 @@ document.getElementById("contactForm").addEventListener("submit", function(event
         console.error('Error:', error);
     });
 });
+/*if (isset($_POST['enviar'])) {
+    if (!empty($_POST['nombre']) && !empty($_POST['email']) && !empty($_POST['tel']) && !empty($_POST['asunto'])) {
+        $name= $_POST ['nombre'];
+        $email= $_POST['email'];
+        $tel= $_POST['tel'];
+        $asunto= $_POST['asunto'];
+        $header= "From: aaronglez302@example.com" . "\r\n";
+        $header="Reply-To: aaronglez302@example.com" . "\r\n";
+        $header= "X-Mailer: PHP/". phpversion();
+        $mail= @mail($email, $tel, $nombre, $asunto, $header);
+        if ($mail) {
+            echo "<h4>¡Mail enviado exitosamente!</h4>";
+        }
+    }
+}*/
